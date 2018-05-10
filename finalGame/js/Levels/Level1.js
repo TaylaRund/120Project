d2acace37d939af17ddcb8b437fe74ad6fc04e13
Emma Yates <epyates@ucsc.edu>
2018-05-10 14:35:10 -0700

    Track camera to a temporary player

--- finalGame/js/Levels/Level1.js
+++ finalGame/js/Levels/Level1.js
@@ -1,20 +1,50 @@
+//globals
+var player;
+
 // define Level1 state and methods
 var Level1 = function(game) {
 };
 Level1.prototype = {
 	preload: function() {
+		game.load.path = './assets/img';
 
+		//temporaries
+		game.load.image('tempBkg', '/temp/sky_1.png');
+		game.load.atlas('atlas', '/temp/atlas.png', '/temp/atlas.json');
 	},
+
 	create: function() {
-    level = 1;
-    game.stage.backgroundColor = "#aaddff"; // set background color
-    var levelText = "Ground Level " + level;
-    var title = game.add.text(game.world.width/2, game.world.height/2, levelText, {fontSize: '48px', fill: '#fff'}); // game title
-    title.anchor.setTo(0.5, 0.5); // set anchor to the middle
+	    level = 1;
+	    game.stage.backgroundColor = "#aaddff"; // set background color
+
+	    //set up background and world
+	    var bkg = game.add.image(0, 0, 'tempBkg');
+	    bkg.scale.y = 1;
+	    bkg.scale.x = game.width/bkg.width;
+
+	    //set world bounds to background size
+	    game.world.setBounds(0, 0, bkg.width, bkg.height);
+
+	    //text
+	    var levelText = "Ground Level " + level;
+	    var title = game.add.text(game.world.width/2, game.world.height/2, levelText, {fontSize: '48px', fill: '#fff'}); // game title
+	    title.anchor.setTo(0.5, 0.5); // set anchor to the middle
+		
+	    //player
+	    player = new PlayerTEMP(game, 'atlas', 'diamond', 1, 0); //PlayerTEMP prefab with scale 1 and rotation 0 -- uses WASD movement
+	    game.add.existing(player);
+	    console.log("WASD movement for Lvl1 TEMP player")
+
+	    //make camera follow player
+	    game.camera.follow(player);
 	},
+
+
 	update: function() {
 		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) { // move on to next state
 			game.state.start('SkyLevel');
 		}
+
+
 	}
 }
--- finalGame/js/Prefabs/temp/PlayerTEMP.js
+++ finalGame/js/Prefabs/temp/PlayerTEMP.js
@@ -1,5 +1,7 @@
+//SIMPLE player prefab used for testing camera movement
+
 // Player prefab constructor function
-function Player(game, key, frame, scale, rotation) {
+function PlayerTEMP(game, key, frame, scale, rotation) {
 	// call to Phaser.Sprite // new Sprite(game, x, y, key, frame)
 	Phaser.Sprite.call(this, game, game.rnd.integerInRange(64,game.width-64), game.rnd.integerInRange(64,game.height-61), key, frame);
 
@@ -14,17 +16,35 @@ function Player(game, key, frame, scale, rotation) {
 	this.body.collideWorldBounds = true;
 	this.body.angularVelocity = game.rnd.integerInRange(-180,180);
 }
-// explicitly define prefab's prototype (Phaser.Sprite) and constructor (Player)
-Player.prototype = Object.create(Phaser.Sprite.prototype);
-Player.prototype.constructor = Player;
+// explicitly define prefab's prototype (Phaser.Sprite) and constructor (PlayerTEMP)
+PlayerTEMP.prototype = Object.create(Phaser.Sprite.prototype);
+PlayerTEMP.prototype.constructor = PlayerTEMP;
 
 // override Phaser.Sprite update (to spin the diamond)
-Player.prototype.update = function() {
+PlayerTEMP.prototype.update = function() {
+	//rotation
 	if(game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
 		this.body.angularVelocity += 5;
 	}
 	if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
 		this.body.angularVelocity -= 5;
 	}
+
+	//WASD movement
+	var speed = 5;
+	if(game.input.keyboard.isDown(Phaser.Keyboard.W)) {
+		this.body.velocity.y -= speed;
+	}	
+	if(game.input.keyboard.isDown(Phaser.Keyboard.S)) {
+		this.body.velocity.y += speed;
+	}
+	if(game.input.keyboard.isDown(Phaser.Keyboard.A)) {
+		this.body.velocity.x -= speed;
+	}
+	if(game.input.keyboard.isDown(Phaser.Keyboard.D)) {
+		this.body.velocity.x += speed;
+	}
+
+
 }
 
