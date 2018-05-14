7236ae5665aca188497b4368a2e01f10c16d3fbc
TaylaRund <35349407+TaylaRund@users.noreply.github.com>
2018-05-13 17:36:08 -0700

    Update SkyLevel.js

--- finalGame/js/Levels/SkyLevel.js
+++ finalGame/js/Levels/SkyLevel.js
@@ -115,7 +115,7 @@ SkyLevel.prototype = {
     },
 
     collectFuel: function(player, Obstacle){
-        player.body.velocity.y -= 100;
+        player.velocity.y -= 100;
         console.log("inside collide Fuel");
         //Obstacle.kill();
     }
