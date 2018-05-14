ca8f9f6136a8214219c20fdc7daeca17955bd49a
TaylaRund <35349407+TaylaRund@users.noreply.github.com>
2018-05-13 20:31:44 -0700

    Update Level1.js
    add mouse sprite

--- finalGame/js/Levels/Level1.js
+++ finalGame/js/Levels/Level1.js
@@ -1,16 +1,24 @@
 // define Level1 state and methods
 var Level1 = function(game) {
+	var player;
+	var max = 500;
 };
 Level1.prototype = {
 	preload: function() {
-
+		game.load.atlas('mouse', 'assets/img/mouse.png', 'assets/img/mouse.json')
+		// load an atlas with a sprite
 	},
 	create: function() {
+		// (game, sprite, grav, level)
+		player = new Player(game, 'mouse', 'mouse01', game.world.height, 'sky', 500, 1000);
+		game.add.existing(player);
+
     level = 1;
     game.stage.backgroundColor = "#aaddff"; // set background color
     var levelText = "Ground Level " + level;
     var title = game.add.text(game.world.width/2, game.world.height/2, levelText, {fontSize: '48px', fill: '#fff'}); // game title
     title.anchor.setTo(0.5, 0.5); // set anchor to the middle
+
 	},
 	update: function() {
 		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) { // move on to next state
