abb771b0172850f66d61b88106e5397789d43a98
emmapy <35883637+emmapy@users.noreply.github.com>
2018-05-25 11:43:37 -0700

    Deleting level 5

--- finalGame/js/Game.js
+++ finalGame/js/Game.js
@@ -14,7 +14,7 @@ game.state.add('Level1', Level1);
 game.state.add('Level2', Level2);
 game.state.add('Level3', Level3);
 game.state.add('Level4', Level4);
-game.state.add('Level5', Level5);
+// game.state.add('Level5', Level5);
 game.state.add('SkyLevel', SkyLevel);
 game.state.add('End', End);
 // START
